cd2ff0cf705d7daaf246a5c15e40e7bd6ce570cd
mchavacalde <167650773+mchavacalde@users.noreply.github.com>
2024-04-20 14:52:34 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,71 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "empty_inventory":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . d d d d d d d . . . . 
+. . . . d 1 1 1 1 1 1 1 d . . . 
+. . . d 1 1 1 1 1 1 1 1 1 d . . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 1 1 1 d . 
+. . . d 1 1 1 1 1 1 1 1 1 d . . 
+. . . . d 1 1 1 1 1 1 1 d . . . 
+. . . . . d d d d d d d . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "MZZ4SWC0?O{`E-u}9gz?":
+            case "inventory_knife":return img`
+........ddddddddd.......
+......dd111111111dd.....
+.....d1111111111111d....
+....d111111111111111d...
+....d1111111111cc111d...
+...d1111111111cdc1111d..
+...d111111111cdc11111d..
+...d11111111cdc111111d..
+...d1111111cdc1111111d..
+...d111111ffc11111111d..
+...d11111fff111111111d..
+...d1111fff1111111111d..
+...d1111ff11111111111d..
+...d11111111111111111d..
+....d111111111111111d...
+....d111111111111111d...
+.....d1111111111111d....
+......dd111111111dd.....
+........ddddddddd.......
+........................
+........................
+........................
+........................
+........................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
