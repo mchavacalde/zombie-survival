8860dc73e50b4034e18680b374243508a158cd87
mchavacalde <167650773+mchavacalde@users.noreply.github.com>
2024-04-27 14:18:56 -0600

    Updates logic

--- main.ts
+++ main.ts
@@ -2,7 +2,22 @@ namespace SpriteKind {
     export const Knife = SpriteKind.create()
     export const InventoryItem = SpriteKind.create()
     export const Portal = SpriteKind.create()
+    export const hiddenWall = SpriteKind.create()
 }
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (sprite.overlapsWith(hiddenWall1)) {
+        showHiddenWallMessage = 1
+    } else if (sprite.overlapsWith(hiddenWall2)) {
+        showHiddenWallMessage = 1
+    } else {
+        showHiddenWallMessage = 0
+    }
+    if (showHiddenWallMessage == 1 && (controller.player1.isPressed(ControllerButton.Up) || controller.player1.isPressed(ControllerButton.Right))) {
+        game.showLongText("There's something here, I can not continue to the portal.", DialogLayout.Bottom)
+        game.showLongText("There must be another path", DialogLayout.Bottom)
+        pause(1000)
+    }
+})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (player_knife >= 1) {
         if (controller.player1.isPressed(ControllerButton.Left)) {
@@ -114,7 +129,28 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
                 ........................
                 `, chimuelo, 200, 0)
         }
+        music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+        sprites.destroy(laser_sword, effects.rings, 100)
     }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Knife, function (sprite, otherSprite) {
+    player_knife = 1
+    sprites.destroy(collectable_knife, effects.blizzard, 100)
+    game.showLongText("You learned the throw knife ability", DialogLayout.Bottom)
+    game.showLongText("Press \"B\" to throw the knife to your enemies", DialogLayout.Bottom)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
+})
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    if (sprite == Enemy1) {
+        enemy1_speed_y = enemy1_speed_y * -1
+        Enemy1.setVelocity(0, enemy1_speed_y)
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
+    sprites.destroy(otherSprite, effects.disintegrate, 200)
+    sprites.destroy(sprite)
+    info.setScore(info.score() + info.life() * 3)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
     if (controller.player1.isPressed(ControllerButton.Up)) {
@@ -127,8 +163,19 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, func
         }
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.jumpDown), music.PlaybackMode.UntilDone)
+    pause(500)
+})
 let laser_sword: Sprite = null
+let showHiddenWallMessage = 0
+let hiddenWall2: Sprite = null
+let hiddenWall1: Sprite = null
+let enemy1_speed_y = 0
+let Enemy1: Sprite = null
 let player_knife = 0
+let collectable_knife: Sprite = null
 let chimuelo: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
 chimuelo = sprites.create(img`
@@ -149,7 +196,7 @@ chimuelo = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-let collectable_knife = sprites.create(img`
+collectable_knife = sprites.create(img`
     ........................
     ........................
     ........................
@@ -182,7 +229,7 @@ controller.moveSprite(chimuelo)
 info.setLife(20)
 chimuelo.setPosition(10, 10)
 scene.cameraFollowSprite(chimuelo)
-let Enemy1 = sprites.create(img`
+Enemy1 = sprites.create(img`
     ........................
     ........................
     ........................
@@ -209,14 +256,12 @@ let Enemy1 = sprites.create(img`
     ........................
     `, SpriteKind.Enemy)
 Enemy1.setPosition(108, 105)
-forever(function () {
-    if (chimuelo.overlapsWith(collectable_knife)) {
-        player_knife = 1
-        sprites.destroy(collectable_knife)
-        game.showLongText("You learned the throw knife ability", DialogLayout.Bottom)
-        game.showLongText("Press \"B\" to throw the knife to your enemies", DialogLayout.Bottom)
-    }
-})
+enemy1_speed_y = 20
+Enemy1.setVelocity(0, enemy1_speed_y)
+hiddenWall1 = sprites.create(assets.image`hidden_wall`, SpriteKind.hiddenWall)
+hiddenWall1.setPosition(85, 8)
+hiddenWall2 = sprites.create(assets.image`hidden_wall`, SpriteKind.hiddenWall)
+hiddenWall2.setPosition(85, 25)
 forever(function () {
     music.play(music.createSong(hex`0078000408010400001c00010a006400f40164000004000000000000000000000000000500000413000000040002272a04000800012408000c00012004001c00100500640000041e000004000000000000000000000000000a04000409000c001000042425272905001c000f0a006400f4010a000004000000000000000000000000000000000213000c001000012a1400180002242518001c00012409010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8000e001800190002080a1c001d00020204`), music.PlaybackMode.UntilDone)
 })
