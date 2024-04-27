0e95c98d14b52a4e9e263cc13dcbf323c2a1a6da
Royner Chavarria <roynercharo@hotmail.com>
2024-04-27 12:53:01 -0600

    Revert "actualizaciones-royner"

--- main.ts
+++ main.ts
@@ -4,89 +4,28 @@ namespace SpriteKind {
     export const Portal = SpriteKind.create()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (player_knife >= 1) {
-        if (controller.player1.isPressed(ControllerButton.Left)) {
-            laser_sword = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . f f . . . 
-                . . . . . . . . . . f c d f . . 
-                . . . . . . . . . f c d f . . . 
-                . . . . f . . . f c d f . . . . 
-                . . . f 7 f . f c d f . . . . . 
-                . . . . f 7 f c d f . . . . . . 
-                . . . . . f 7 f f . . . . . . . 
-                . . . . f e f 7 f . . . . . . . 
-                . . . f e f . f 7 f . . . . . . 
-                . . f e f . . . f . . . . . . . 
-                . f e f . . . . . . . . . . . . 
-                . . f . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, chimuelo, -200, 0)
-        } else if (controller.player1.isPressed(ControllerButton.Up)) {
-            laser_sword = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . f f . . . 
-                . . . . . . . . . . f c d f . . 
-                . . . . . . . . . f c d f . . . 
-                . . . . f . . . f c d f . . . . 
-                . . . f 7 f . f c d f . . . . . 
-                . . . . f 7 f c d f . . . . . . 
-                . . . . . f 7 f f . . . . . . . 
-                . . . . f e f 7 f . . . . . . . 
-                . . . f e f . f 7 f . . . . . . 
-                . . f e f . . . f . . . . . . . 
-                . f e f . . . . . . . . . . . . 
-                . . f . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, chimuelo, 0, -200)
-        } else if (controller.player1.isPressed(ControllerButton.Down)) {
-            laser_sword = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . f f . . . 
-                . . . . . . . . . . f c d f . . 
-                . . . . . . . . . f c d f . . . 
-                . . . . f . . . f c d f . . . . 
-                . . . f 7 f . f c d f . . . . . 
-                . . . . f 7 f c d f . . . . . . 
-                . . . . . f 7 f f . . . . . . . 
-                . . . . f e f 7 f . . . . . . . 
-                . . . f e f . f 7 f . . . . . . 
-                . . f e f . . . f . . . . . . . 
-                . f e f . . . . . . . . . . . . 
-                . . f . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, chimuelo, 0, 200)
-        } else {
-            laser_sword = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . f f . . . 
-                . . . . . . . . . . f c d f . . 
-                . . . . . . . . . f c d f . . . 
-                . . . . f . . . f c d f . . . . 
-                . . . f 7 f . f c d f . . . . . 
-                . . . . f 7 f c d f . . . . . . 
-                . . . . . f 7 f f . . . . . . . 
-                . . . . f e f 7 f . . . . . . . 
-                . . . f e f . f 7 f . . . . . . 
-                . . f e f . . . f . . . . . . . 
-                . f e f . . . . . . . . . . . . 
-                . . f . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, chimuelo, 200, 0)
-        }
-    }
+    laser_sword = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . f f . . . 
+        . . . . . . . . . . f c d f . . 
+        . . . . . . . . . f c d f . . . 
+        . . . . f . . . f c d f . . . . 
+        . . . f 7 f . f c d f . . . . . 
+        . . . . f 7 f c d f . . . . . . 
+        . . . . . f 7 f f . . . . . . . 
+        . . . . f e f 7 f . . . . . . . 
+        . . . f e f . f 7 f . . . . . . 
+        . . f e f . . . f . . . . . . . 
+        . f e f . . . . . . . . . . . . 
+        . . f . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, chimuelo, 100, 0)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
     if (controller.player1.isPressed(ControllerButton.Right)) {
-        if (player_knife) {
+        if (cuchillo) {
             game.gameOver(true)
             game.setGameOverEffect(true, effects.confetti)
             game.setGameOverMessage(true, "YOU WIN!")
@@ -96,7 +35,7 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, func
     }
 })
 let laser_sword: Sprite = null
-let player_knife = 0
+let cuchillo = 0
 let chimuelo: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
 chimuelo = sprites.create(img`
@@ -152,8 +91,9 @@ let collectable_knife = sprites.create(img`
     ........................
     `, SpriteKind.Knife)
 collectable_knife.setPosition(30, 120)
-player_knife = 0
-let player_sword = 0
+cuchillo = 0
+let espada = 0
+let portal_touched = 0
 controller.moveSprite(chimuelo)
 info.setLife(20)
 chimuelo.setPosition(10, 10)
@@ -161,9 +101,9 @@ music.play(music.stringPlayable("C5 B A G G A B C5 ", 130), music.PlaybackMode.L
 scene.cameraFollowSprite(chimuelo)
 forever(function () {
     if (chimuelo.overlapsWith(collectable_knife)) {
-        player_knife = 1
+        cuchillo = 1
         sprites.destroy(collectable_knife)
-        game.showLongText("You learned the \"throw knife\" ability", DialogLayout.Bottom)
+        game.showLongText("You learned the throw knife ability", DialogLayout.Bottom)
         game.showLongText("Press \"A\" to throw the knife to your enemies", DialogLayout.Bottom)
     }
 })
